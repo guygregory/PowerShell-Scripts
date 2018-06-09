ecdcbdf1de9ed02cfe2ed20399cf8aaf07a0c889
Guy Gregory <register@guygregory.com>
2018-06-09 21:09:19 +0100

    Delete Get-Country.ps1

--- Get-Country.ps1
+++
@@ -1,4 +0,0 @@
-function Get-Country
-{
-([xml](New-Object Net.WebClient).DownloadString('http://www.webservicex.net/geoipservice.asmx/GetGeoIP?IPAddress='+(New-Object Net.WebClient).DownloadString('http://ip.appspot.com'))).GeoIP.CountryName
-}
